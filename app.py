9eaf5825cbd2720c5cdc98225342c1ea78d44d35
viral3899 <viralsherathiya1008@gmail.com>
2023-03-22 08:49:34 +0530

    exception updated

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+import sys
+import os
+
 from flask import Flask,request,render_template
+from housing.exception.exception import HousingException
+from housing.logger.logger import logging
+
 # import pandas as pand
 
 app=Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    try:
+        raise Exception("Exception Testing")
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+
     return """
     <h1>Start Machine Learning Project</h1>
     """
--- housing/exception/exception.py
+++ housing/exception/exception.py
@@ -1,19 +1,33 @@
 import sys
 
-def error_message_details(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    line_no=exc_tb.tb_lineno
-    error_message=f"Error Occured in Python Script [{file_name}] line number [{line_no}] error message [{str(error)}]"
-
-    return error_message
 
 
 
-class CustomeException(Exception):
-    def __init__(self, error_message,error_detail:sys):
+
+class HousingException(Exception):
+    
+    def __init__(self, error_message:Exception,error_detail:sys):
         super().__init__(error_message)
-        self.error_message=error_message_details(error_message,error_detail=error_message_details)
+        self.error_message=HousingException.get_detailed_error_message(error=error_message,error_detail=error_detail)
+
+
+    @staticmethod
+    def get_detailed_error_message(error,error_detail:sys) ->str:
+        _,_,exc_tb=error_detail.exc_info()
+        file_name=exc_tb.tb_frame.f_code.co_filename
+        try_block_line_no=exc_tb.tb_lineno
+        EXception_block_line_no=exc_tb.tb_frame.f_lineno
+        error_message=f"""
+        Error Occured in Python Script : [{file_name}] 
+        at try block line number : [{try_block_line_no}]
+        and exception block line no : [{EXception_block_line_no}]
+        error message : [{str(error)}]
+        """
+
+        return error_message
     
     def __str__(self):
         return self.error_message
+        
+    def __repr__(self) -> str:
+        return HousingException.__name__.str()
