c3e236c64ca87516c67ab4c85cf0afe411396bca
viral3899 <viralsherathiya1008@gmail.com>
2023-03-26 01:21:35 +0530

    Data_ingestion_config_added

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -11,15 +11,76 @@ from housing.entity.config_entity import DataIngestionConfig, DataTransforamtion
     ModelEvaluationConfig, ModelPusherConfig, ModelTrainerConfig, TrainingPipelineConfig
 
 
-class ConConfiguration:
+class Configuration:
     def __init__(self, config_file_path=CONFIG_FILE_PATH,
                  current_time_stamp=get_current_time_stamp()) -> None:
+
         self.config_info = read_yaml_file(file_path=config_file_path)
         self.time_stamp = current_time_stamp
         self.training_pipeline_cofig = self.get_training_pipeline_config()
+        self.data_ingestion_config = self.get_data_ingestion_config()
+
+    def get_training_pipeline_config(self) -> TrainingPipelineConfig:
+        try:
+            training_pipline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+            artifact_dir = os.path.join(
+                ROOT_DIR, training_pipline_config[TRAINING_PIPELINE_NAME_KEY],
+                training_pipline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])
+            training_pipline_config = TrainingPipelineConfig(
+                artifact_dir=artifact_dir)
+
+            logging.info(
+                f"Training Pipeline Config: {training_pipline_config}")
+
+            return training_pipline_config
+        
+        except Exception as e:
+            logging.info(f"Error Occured at {HousingException(e,sys)}")
+            raise HousingException(e, sys)
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
-        pass
+        try:
+            data_ingestion_info=self.config_info[DATA_INGESTION_CONFIG_KEY]
+
+            dataset_download_url=data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
+
+            artifact_dir=self.training_pipeline_cofig.artifact_dir
+            data_ingestion_artifact_dir=os.path.join(artifact_dir,DATA_INGESTION_ARTIFACT_DIR,
+                                                     get_current_time_stamp())
+            
+
+            raw_data_dir=os.path.join(data_ingestion_artifact_dir,
+                                      data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY])
+            
+            ingested_dir=os.path.join(data_ingestion_artifact_dir,
+                                      data_ingestion_info[DATA_INGESTION_INGESTED_DIR_KEY])
+            
+            tgz_download_dir=os.path.join(data_ingestion_artifact_dir,
+                                      data_ingestion_info[DATA_INGESTION_INGESTED_DIR_KEY])
+
+            ingested_train_dir=os.path.join(ingested_dir,data_ingestion_info[DATA_INGESTION_INGESTED_TRAIN_DIR_KEY])
+
+            ingested_test_dir=os.path.join(ingested_dir,data_ingestion_info[DATA_INGESTION_INGESTED_TEST_DIR_KEY])
+
+            
+
+            # tgz_download_dir=(data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY])
+
+            data_ingestion_config = DataIngestionConfig(
+                dataset_download_url=dataset_download_url,
+                tgz_download_dir=tgz_download_dir,
+                raw_data_dir=raw_data_dir,
+                ingested_train_dir=ingested_train_dir,
+                ingested_test_dir=ingested_test_dir
+            )
+
+
+            logging.info(f"Data Ingestion Config: {data_ingestion_config}")
+            return data_ingestion_config
+
+        except Exception as e:
+            logging.info(f"Error Occured at {HousingException(e,sys)}")
+            raise HousingException(e, sys)
 
     def get_data_validation_config(self) -> DataValidationConfig:
         pass
@@ -35,18 +96,3 @@ class ConConfiguration:
 
     def get_model_pusher_config(self) -> ModelPusherConfig:
         pass
-
-    def get_training_pipeline_config(self) -> TrainingPipelineConfig:
-        try:
-            training_pipline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
-            artifact_dir = os.path.join(
-                ROOT_DIR, training_pipline_config[TRAINING_PIPELINE_NAME_KEY], training_pipline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])
-            training_pipline_config = TrainingPipelineConfig(
-                artifact_dir=artifact_dir)
-
-            logging.info(
-                f"Training Pipeline Config: {training_pipline_config}")
-
-        except Exception as e:
-            logging.info(f"Error Occured at {HousingException(e,sys)}")
-        raise HousingException(e, sys)
--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -11,11 +11,23 @@ CONFIG_DIR='config'
 CONFIG_FILE_NAME="config.yaml"
 CONFIG_FILE_PATH=os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
 
-#Taining Pipeline Config Constants
+#Training Pipeline Config Constants
 
 TRAINING_PIPELINE_CONFIG_KEY='training_pipeline_config'
 TRAINING_PIPELINE_NAME_KEY='pipeline_name'
 TRAINING_PIPELINE_ARTIFACT_DIR_KEY='artifact_dir'
+
+
+#Data Ingestion Pipeline Config Constants
+DATA_INGESTION_ARTIFACT_DIR='data_ingestion'
+DATA_INGESTION_CONFIG_KEY='data_ingestion_config'
+DATA_INGESTION_DOWNLOAD_URL_KEY='dataset_download_url'
+DATA_INGESTION_RAW_DATA_DIR_KEY ='raw_data_dir'
+DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY='tgz_download_dir'
+DATA_INGESTION_INGESTED_DIR_KEY="ingested_dir"
+DATA_INGESTION_INGESTED_TRAIN_DIR_KEY="ingested_train_dir"
+DATA_INGESTION_INGESTED_TEST_DIR_KEY="ingested_test_dir"
+
 
 
 
