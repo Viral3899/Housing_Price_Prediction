b1bfa52d2552127689816faf80f4b2e60d9b71ff
Virl3899 <viralsherathiya1008@gmail.com>
2023-03-27 14:35:03 +0530

    added

--- housing/logger/logger.py
+++ housing/logger/logger.py
@@ -21,29 +21,3 @@ logging.basicConfig(filename=LOG_FILE_PATH,
                     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
                     level=logging.INFO,
                     )
-
-
-
-# import logging
-# from datetime import datetime
-# import os
-# import pandas as pd
-# from housing.constant import get_current_time_stamp 
-# LOG_DIR="logs"
-
-# def get_log_file_name():
-#     return f"log_{get_current_time_stamp()}.log"
-
-# LOG_FILE_NAME=get_log_file_name()
-
-# os.makedirs(LOG_DIR,exist_ok=True)
-
-# LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
-
-
-
-# logging.basicConfig(filename=LOG_FILE_PATH,
-# filemode="w",
-# format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
-# level=logging.INFO
-# )
