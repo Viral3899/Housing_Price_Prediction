fb4b770910feeb635d6b646e2722888f678b805e
Virl3899 <viralsherathiya1008@gmail.com>
2023-03-27 12:40:08 +0530

    added

--- demo.py
+++ demo.py
@@ -3,12 +3,15 @@ from housing.logger.logger import logging
 from housing.exception.exception import HousingException
 import sys
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 def main():
     try:
         pipe=Pipeline()
         pipe.run_pipeline()
     except Exception as e:
-            logging.info(f"Error Occured at {HousingException(e,sys)}")
+            logging.error(f"Error Occured at {HousingException(e,sys)}")
             raise HousingException(e,sys) from e 
 
 
