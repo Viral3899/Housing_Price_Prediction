79e965a773d901906b99b415d1450a3a25cf2c29
viral3899 <viralsherathiya1008@gmail.com>
2023-04-05 08:37:44 +0530

    added

--- demo.py
+++ demo.py
@@ -16,6 +16,7 @@ def main():
     except Exception as e:
         logging.error(f"Error Occurred at {HousingException(e,sys)}")
         raise HousingException(e, sys)
+    
 if __name__ == '__main__':
 
     main()
