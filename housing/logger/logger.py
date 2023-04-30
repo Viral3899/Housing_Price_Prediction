6a3730eb5e25f1f66adc551e6d9cf74b8a8e1a6a
viral3899 <viralsherathiya1008@gmail.com>
2023-04-30 11:45:38 +0530

    updated

--- housing/logger/logger.py
+++ housing/logger/logger.py
@@ -20,6 +20,6 @@ LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
 
 logging.basicConfig(filename=LOG_FILE_PATH,
                     filemode='w',
-                    format="[%(asctime)s] - %(filename)s - %(lineno)d - %(name)s - %(funcName)s() - %(levelname)s - %(message)s",
+                    format="[%(asctime)s] || %(filename)s || %(lineno)d || %(name)s || %(funcName)s() || %(levelname)s -->> %(message)s",
                     level=logging.INFO,
                     )
